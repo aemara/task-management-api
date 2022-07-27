503dbaf52869a30758ea332d7f4e2ab96043dabb
Amr Emara <a.emara47@gmail.com>
2022-07-27 14:17:29 +0200

    remove testing code

--- app.js
+++ app.js
@@ -10,55 +10,6 @@ const port = 3000;
 
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-    const subTask = new Subtask({
-        name: 'this is a subtask'
-    });
-
-    const subTasksList = [];
-    subTasksList.push(subTask);
-    console.log(subTasksList);
-
-
-    const task = new Task({
-        title: 'Finish the app',
-        description: 'A fullstack  app for resume',
-        subtasks:  subTasksList,
-        status: 'Todo'
-    });
-
-    const task2 = new Task({
-        title: 'Read a book',
-        description: 'read Sense and Sensibility',
-        subtasks:  subTasksList,
-        status: 'Doing'
-    });
-
-    const tasksList = [];
-    tasksList.push(task);
-    tasksList.push(task2);
-
-    
-    const column = new Column({
-        title: 'this is a column',
-        tasks: tasksList,
-        numOfTasks: tasksList.length
-
-    });
-
-    const columnsList = [];
-    columnsList.push(column);
-
-    console.log(column);
-    console.log(column.tasks);
-    console.log(column.tasks[0].subtasks);
-
-    const board = new Board({
-        title: 'This is a board',
-        columns: columnsList
-    });
-
-    console.log(board);
     
   })
   
