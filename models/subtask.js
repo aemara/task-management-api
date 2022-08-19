f9842851e76a39260043f458a6a13e2886b82351
Amr Emara <a.emara47@gmail.com>
2022-08-19 19:17:51 +0200

    change subtask schema

--- models/subtask.js
+++ models/subtask.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const subtaskSchema = mongoose.Schema({
   name: { type: String, required: true },
   taskId: { type: String },
+  columnId: {type: String}
 });
 const subtaskModel = mongoose.model("Subtask", subtaskSchema);
 
