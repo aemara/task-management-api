9cada54eb329ab9c3591586b68e2325355456181
Amr Emara <a.emara47@gmail.com>
2022-09-29 05:46:54 +0200

    add columns controllers and routes

--- controllers/columns.js
+++ controllers/columns.js
@@ -1,53 +1,43 @@
-const { Board } = require("./models/board");
-const { Column } = require("./models/column");
-const { Task } = require("./models/task");
-const { Subtask } = require("./models/subtask");
-/**
- *
- * A POST endpoint for adding a column to a board
- */
-app.post("/addcolumn/:boardId", (req, res) => {
-  const boardId = req.params["boardId"];
-  const column = new Column({
-    title: req.body.title,
-  });
+const { Board } = require("../models/board");
+const { Column } = require("../models/column");
 
-  Board.findById(boardId, (err, board) => {
-    column.save();
-    board.columns.push(column);
-    board.save();
-    res.status(201).json({
+module.exports = {
+  addColumn: async (req, res) => {
+    console.log(`request to add column`);
+    const boardId = req.params["boardId"];
+    const column = new Column({
+      title: req.body.title,
+    });
+
+    const board = await Board.findById(boardId);
+    const savedColumn = await column.save();
+    board.columns.push(savedColumn);
+    await board.save();
+    res.status(200).json({
       message: "A board was found and a column was added",
     });
-  });
-});
+  },
 
-/**
- * GET endpoint for fetching the columns of a board
- */
+  getColumns: (req, res) => {
+    console.log(`request to get columns`);
+    Board.findById(req.params["boardId"])
+      .populate({
+        path: "columns",
+      })
+      .exec((err, board) => {
+        res.status(200).json({
+          boardTitle: board.title,
+          columns: board.columns,
+        });
+      });
+  },
 
-app.get("/columns/:boardId", (req, res) => {
-  Board.findById(req.params["boardId"])
-    .populate({
-      path: "columns",
-    })
-    .exec((err, board) => {
+  getColumn: (req, res) => {
+    Column.find({ _id: req.params["columnId"] }).then((column) => {
       res.status(200).json({
-        boardTitle: board.title,
-        columns: board.columns,
+        message: "Column fetched successfully",
+        column: column,
       });
     });
-});
-
-/**
- * GET endpoint for fetching a column
- */
-
-app.get("/column/:columnId", (req, res) => {
-  Column.find({ _id: req.params["columnId"] }).then((column) => {
-    res.status(200).json({
-      message: "Column fetched successfully",
-      column: column,
-    });
-  });
-});
+  },
+};
--- routes/columns.js
+++ routes/columns.js
@@ -2,9 +2,9 @@ const express = require("express");
 const router = express.Router();
 const columnsController = require('../controllers/columns');
 
-router.post('/addcolumn', columnsController.addColumn);
-router.get('/getcolumns', columnsController.getColumns);
-router.get('/getcolumn', columnsController.getColumn);
+router.post('/addcolumn/:boardId', columnsController.addColumn);
+router.get('/getcolumns/:boardId', columnsController.getColumns);
+router.get('/getcolumn/:columnId', columnsController.getColumn);
 
 
 module.exports = router;
