6568486a21ffd89a36d47bcf98e75b5c59eebcb5
Amr Emara <a.emara47@gmail.com>
2022-10-13 16:21:24 +0200

    add async/await to a subtask controller

--- controllers/subtasks.js
+++ controllers/subtasks.js
@@ -11,25 +11,24 @@ module.exports = {
     });
   },
 
-  toggleDone: (req, res) => {
+  toggleDone: async (req, res) => {
     const subtaskId = req.params["subtaskId"];
     const taskId = req.params["taskId"];
 
-    Task.findOne({ _id: taskId }, (err, task) => {
-      task.subtasks.forEach((subtask) => {
-        if (subtask._id.toString() === subtaskId) {
-          if (subtask.done) {
-            subtask.done = false;
-          } else {
-            subtask.done = true;
-          }
+    const task = await Task.findOne({ _id: taskId });
+    for (var subtask of task.subtasks) {
+      if (subtask._id.toString() === subtaskId) {
+        if (subtask.done) {
+          subtask.done = false;
+        } else {
+          subtask.done = true;
         }
-      });
-      task.save().then(() => {
-        res.status(200).json({
-          message: "Subtask's status changed",
-        });
-      });
+      }
+    }
+
+    await task.save();
+    res.status(200).json({
+      message: "Subtask's status changed",
     });
   },
 };
